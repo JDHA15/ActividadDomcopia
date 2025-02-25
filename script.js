ea81f6a30e5105fa0e6d0b363fefe1381ddd39fa
Roberto Sánchez <roberto.sanchez2024@hilahasl.com>
2025-02-25 08:44:59 -0600

    consejos para las clases con Teacher Irvin

---
+++ script.js
@@ -0,0 +1,42 @@
+const general = document.getElementById('contenido')
+general.innerHTML = '<h1>Hola jovenes de la tarde, aqui unos consejos para las clases con teacher Irvin</h1>'
+ 
+const elemento = document.createElement('h3')
+elemento.innerHTML = 'Mantengan una mentalidad abierta:.'
+general.appendChild(elemento)
+ 
+const elemento1 = document.createElement('h4')
+elemento1.innerHTML = 'Cambiar de profesor puede ser un reto, pero también es una oportunidad para aprender de una nueva forma. Traten de ver el cambio como algo positivo.'
+general.appendChild(elemento1)
+ 
+const segundo = document.createElement('h3')
+segundo.innerHTML = 'Sean proactivos en las clases: '
+general.appendChild(segundo)
+ 
+const segundo1 = document.createElement('h4')
+segundo1.innerHTML = 'Él teacher es muy activo y parece una cajita de sorpresas, es importante estar siempre preparado. Participen en las actividades, ya que estas podrían contar para la evaluación.'
+general.appendChild(segundo1)
+ 
+const tercero = document.createElement('h3')
+tercero.innerHTML= 'Tomen buenos apuntes:'
+general.appendChild(tercero)
+ 
+const tercero1 = document.createElement('h4')
+tercero1.innerHTML= 'La clase activa y dinámica podría cubrir mucho contenido en poco tiempo. Es fundamental que tomen apuntes detallados durante las clases, ya que esto te ayudará a repasar y entender los temas más fácilmente.'
+general.appendChild(tercero1)
+ 
+const cuarto = document.createElement('h3')
+cuarto.innerHTML= 'Hagan preguntas cuando no entiendan:'
+general.appendChild(cuarto)
+ 
+const cuarto1 = document.createElement('h4')
+cuarto1.innerHTML= 'No duden en aclarar cualquier duda que tengan. El teacher probablemente prefiere que pregunten y demuestren interés a que se queden con la duda.'
+general.appendChild(cuarto1)
+ 
+const quinto = document.createElement('h3')
+quinto.innerHTML= 'Mantengan la calma y no te frustres:'
+general.appendChild(quinto)
+ 
+const quinto1 = document.createElement('h4')
+quinto1.innerHTML= 'Adaptarse al nuevo estilo del Teacher puede ser difícil, pero mantengan la calma. Si alguna actividad no les sale bien, aprendan de ella y sigan adelante, ya que el Teacher Irvin probablemente valora el esfuerzo y la mejora continua.'
+general.appendChild(quinto1)
